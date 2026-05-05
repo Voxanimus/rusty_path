73dc33c0ddb1d6a8f30707a64cbe6edf41a1fb25
Voxanimus <louis.pepito.dumas@protonmail.com>
2026-05-05 10:22:00 +0200

    Finished chapter 12

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -1,31 +1,33 @@
-use std::env;
-use std::fs;
-use std::process;
-use std::error::Error;
-use minigrep_12::search;
+use minigrep_12::{search, search_case_insensitive};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config){
-        println!("Application error: {e}");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents =
-        fs::read_to_string(config.file_path)?;
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -33,8 +35,9 @@ fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -45,10 +48,12 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query: query,
             file_path: file_path,
+            ignore_case,
         })
     }
 }
