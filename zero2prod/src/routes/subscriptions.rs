49a405bbb9ffc3a1379ef320ef512d4f5fea84f4
Voxanimus <louis.pepito.dumas@protonmail.com>
2026-08-12 16:50:10 +0200

    Add input validation of username

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -2,7 +2,10 @@ use axum::{Form, extract::State};
 use chrono::Utc;
 use reqwest::StatusCode;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
+
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -19,25 +22,48 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(State(pool): State<PgPool>, Form(form): Form<FormData>) -> StatusCode {
-    match insert_subscriber(&pool, &form).await {
+    let name = match SubscriberName::parse(form.name) {
+        Ok(name) => name,
+        Err(_) => return StatusCode::BAD_REQUEST,
+    };
+
+    let new_subcriber = NewSubscriber {
+        email: form.email,
+        name,
+    };
+
+    match insert_subscriber(&pool, &new_subcriber).await {
         Ok(_) => StatusCode::OK,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbiden_character = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contain_forbidden_characters = s.chars().any(|g| forbiden_character.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contain_forbidden_characters)
+}
+
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
